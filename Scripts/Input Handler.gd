d9f5aa14e012b71683319970f2bc525f25f1637a
McBrosum <101577648+McBrosum@users.noreply.github.com>
2024-10-02 19:53:33 -0400

    Update Input Handler.gd

--- Scripts/Input Handler.gd
+++ Scripts/Input Handler.gd
@@ -22,7 +22,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	# only check if the user's text has any character that is not a letter
-	if !is_text_valid(user_line.text):
+	if user_line.text.length() == 0:
 		# reset user text and check again
 		user_line.text = ""
 		return
@@ -67,23 +67,23 @@ func _process(_delta):
 
 
 ## Utility
-# checks string
-# O(n) time
-# O(1) space
+# checks if the currrent string is only letters
 func is_alpha(text: String):
 	const ALPHABET: Dictionary = {
-		"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9,
-		"k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19,
-		"u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25,
-		"A": 26, "B": 27, "C": 28, "D": 29, "E": 30, "F": 31, "G": 32, "H": 33, "I": 34, "J": 35,
-		"K": 36, "L": 37, "M": 38, "N": 39, "O": 40, "P": 41, "Q": 42, "R": 43, "S": 44, "T": 45,
-		"U": 46, "V": 47, "W": 48, "X": 49, "Y": 50, "Z": 51
+		'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9,
+		'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19,
+		'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25,
+		'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35,
+		'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45,
+		'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51
 	}
 	
 	for c in text:
-		if ALPHABET.has(str(c)):
+		if ALPHABET.has(c):
 			return true
 	return false
+
+# checks if the string is only digits
 func is_digit(text: String):
 	const NUMBERS: Dictionary = {
 		"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9
@@ -93,6 +93,8 @@ func is_digit(text: String):
 		if NUMBERS.has(c):
 			return true
 	return false
+
+# checks if the string is only symbols
 func is_symbol(text: String):
 	const SYMBOLS: Dictionary = {
 		0: "!", 1: "@", 2: "#", 3: "$", 4: "%", 5: "^", 6: "&", 7: "*", 8: "(", 9: ")", 10: "-", 11: "_", 12: "+", 13: "=", 14: "[", 15: "]", 16: "{", 17: "}", 18: ";", 19: "'", 20: ":", 21: "\"", 22: ",", 23: "<", 24: ".", 25: ">", 26: "/", 27: "?",
@@ -101,10 +103,4 @@ func is_symbol(text: String):
 	for c in text:
 		if SYMBOLS.has(c):
 			return true
-	return false
-
-# checks the user input
-func is_text_valid(new_text):
-	if new_text.length() > 0:
-		return true
 	return false
