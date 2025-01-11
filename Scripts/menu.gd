89fa1ba48db2d6156323afe15ef43823490e6674
Peyton Robinson <lvlr.iviythos@gmail.com>
2025-01-11 07:33:16 -0500

    Revert "Finished Settings And Basic Game"
    This reverts commit 6d9e1513833c0422e73635313a6192d5c1e922bd.

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 
 @onready var user_input: LineEdit = $Buttons/user_input
-@onready var page_list = $"Buttons/page list"
 
 
 # init
@@ -12,6 +11,7 @@ func _ready() -> void:
 
 # user enters text
 func _on_user_input_text_submitted(new_text: String) -> void:
+	
 	# main scenes
 	var two_letters: String = new_text.substr(0, 2).to_lower()
 	if (two_letters == "pl"):
@@ -23,6 +23,7 @@ func _on_user_input_text_submitted(new_text: String) -> void:
 	elif (two_letters == "qu"):
 		get_tree().quit()
 	else:
+		
 		# custom input
 		user_input.text = ""
 		if (new_text == "TYPIC"):
@@ -36,18 +37,3 @@ func _on_user_input_text_submitted(new_text: String) -> void:
 # changes placeholder text if the user types
 func _on_user_input_text_changed(new_text: String) -> void:
 	user_input.placeholder_text = "Type the page here"
-
-
-func _on_page_list_item_clicked(index, at_position, mouse_button_index):
-	match index:
-		0:
-			get_tree().change_scene_to_file("res://Scenes/game.tscn")
-		1:
-			get_tree().change_scene_to_file("res://Scenes/settings.tscn")
-		2:
-			get_tree().change_scene_to_file("res://Scenes/score.tscn")
-		3:
-			get_tree().quit()
-		'_':
-			print("Did not click any buttons.");
-			get_tree().quit()
--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -4,27 +4,27 @@ extends Node2D
 
 ## button variables
 # array item indexes
-# 0 - easy
-# 1 - medium
-# 2 - hard
-# 3 - pro
+# 1 - easy
+# 2 - medium
+# 3 - hard
+# 4 - pro
 @onready var difficulty: ItemList = $Node/Difficulty
 
-# 0 - lowercase
-# 1 - uppercase
-# 2 - numbers
-# 3 - symbols
+# 1 - lowercase
+# 2 - uppercase
+# 3 - numbers
+# 4 - symbols
 @onready var text: ItemList = $Node/Text
 
-# 0 - Music
-# 1 - Sound
+# 1 - Music
+# 2 - Sound
 @onready var sound: ItemList = $Node/Sound
 
-# 0 - current char
-# 1 - missed char
-# 2 - correct char
-# 3 - hard char
-# 4 - Accuracy
+# 1 - current char
+# 2 - missed char
+# 3 - correct char
+# 4 - hard char
+# 5 - Accuracy
 @onready var score_display: ItemList = $"Node/Score Display"
 
 ## other variables
@@ -39,33 +39,22 @@ func _on_menu_button_pressed() -> void:
 
 ## settings buttons
 # score list
-# changes the scores displayed - descritpion found in data.gd - using the clicked item index.
 func _on_score_display_multi_selected(index: int, selected: bool) -> void:
 	data.display_scores[index] = !data.display_scores[index]
+	print(data.display_scores)
 	pass
 
 
-# text
-# changes the text modifier array - description found in data.gd - using the clicked item index.
-func _on_text_item_clicked(index, at_position, mouse_button_index):
-	data.text_modifiers[index] = !data.text_modifiers[index]
-
-
 # difficulty
-# changes the difficulty using the clicked item index.
-func _on_difficulty_item_clicked(index, at_position, mouse_button_index):
-	match index:
-		0:
-			data.difficulty = data.Difficulty.EASY
-		1:
-			data.difficulty = data.Difficulty.MEDIUM
-		2:
-			data.difficulty = data.Difficulty.HARD
-		3:
-			data.difficulty = data.Difficulty.PRO
+func _on_difficulty_item_selected(index: int) -> void:
+	pass # Replace with function body.
+
+
+# text
+func _on_text_item_selected(index: int) -> void:
+	pass # Replace with function body.
 
 
 # sound
-# changes the sound options array - description found in data.gd - using the cilcked item index
-func _on_sound_item_clicked(index, at_position, mouse_button_index):
-	data.sound_modifiers[index] = !data.sound_modifiers[index]
+func _on_sound_item_selected(index: int) -> void:
+	pass # Replace with function body.
